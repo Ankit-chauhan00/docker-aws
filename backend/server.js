693f094569a40a0c00e987546660c1aed4bc08c2
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-06-24 08:18:09 +0530

    socket io and express basic server setup

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,34 @@ import { Server } from "socket.io"
 import {YSocketIO} from "y-socket.io/dist/server"
 
 
-const app. = 
+const app = express()
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+    cors:{
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+const ySocketIO =  new YSocketIO(io)
+ySocketIO.initialize()
+
+app.get("/",(req, res)=>{
+    res.status(200).json({
+        message: "hello world",
+        success: true
+    })
+})
+
+app.get("/", (req,res)=>{
+    res.status(200).json({
+        message: "ok",
+        success: true
+    })
+})
+
+
+httpServer.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
