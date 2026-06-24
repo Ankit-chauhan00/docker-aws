e45bf1db52570301d0edf412f10748d9ff4cafc6
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-06-24 08:03:19 +0530

    added backend dependencies

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import "./App.css";
+import { Editor } from "@monaco-editor/react";
 const App = () => {
-  return <main className="">dqwdwq</main>;
+  return (
+    <main className="h-screen w-full bg-gray-950 flex gap-4 p-3">
+      <aside className="h-full w-1/5 bg-amber-200 rounded-md"></aside>
+
+      <section className="h-full w-4/5 bg-amber-700 rounded-md overflow-hidden">
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          defaultValue="// some comments"
+          theme="vs-dark"
+        />
+      </section>
+    </main>
+  );
 };
 
 export default App;
