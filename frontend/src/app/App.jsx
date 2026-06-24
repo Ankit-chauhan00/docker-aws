a78d29227765e531370ce6b1dbb37588c2a7e0a3
Ankit-chauhan00 <ankitchauhan820066@gmail.com>
2026-06-24 08:41:23 +0530

    implemented the socket io with yjs

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useRef, useMemo } from "react";
 import "./App.css";
 import { Editor } from "@monaco-editor/react";
+import { MonacoBinding } from "y-monaco";
+import * as Y from "yjs";
+import { SocketIOProvider } from "y-socket.io";
+
 const App = () => {
+  const editorRef = useRef(null);
+
+  const ydoc = useMemo(() => new Y.Doc(), []);
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc]);
+
+  const handleMount = (editor) => {
+    editorRef.current = editor;
+    const provider = new SocketIOProvider(
+      "http://localhost:3000",
+      "monaco",
+      ydoc,
+      {
+        autoConnect: true,
+      }
+    );
+    const monacoBinding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    );
+  };
   return (
     <main className="h-screen w-full bg-gray-950 flex gap-4 p-3">
       <aside className="h-full w-1/5 bg-amber-200 rounded-md"></aside>
@@ -12,6 +38,7 @@ const App = () => {
           defaultLanguage="javascript"
           defaultValue="// some comments"
           theme="vs-dark"
+          onMount={handleMount}
         />
       </section>
     </main>
